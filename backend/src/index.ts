f93ac3bbebb619515ea12babaf93542d849f36b1
DevBroParas <parasprofessionaly@gmail.com>
2026-03-02 18:08:28 +0530

    #7_frontend_backend=changing env of both frontend and backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 app.use(express.json());
